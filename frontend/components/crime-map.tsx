73bcdd2c5d2f1212ae0153614a8d14c8711fb577
NiccoloComati <76107977+NiccoloComati@users.noreply.github.com>
2026-03-11 14:26:19 -0400

    Switch map basemap to CARTO voyager

--- frontend/components/crime-map.tsx
+++ frontend/components/crime-map.tsx
@@ -182,7 +182,7 @@ export default function CrimeMap({ payload }: CrimeMapProps) {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
         <GeoJSON
           data={payload.geojson as never}
